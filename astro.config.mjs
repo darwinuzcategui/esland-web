fcc2e9cebf9d9f066fb9966092a95f7fda34b884
Darwin Uzcategui pc windows <darwin.uzcategui1973@hotmail.com>
2023-12-29 16:54:57 -0400

    desploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https:///darwinuzcategui/.github.io',
+  base: '/esland-web',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'ca'],
